23a834243f2aeb403f9c62824101bc60de34b4b9
Nidhi shakya <nidhikalpana555@gmail.com>
2025-07-19 19:37:24 +0530

    done

--- BACKEND/model/usermodel.js
+++ BACKEND/model/usermodel.js
@@ -1,38 +1,43 @@
 const mongoose = require('mongoose')
+
 const userschema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-     email:{
-        type:String,
-        required:true,
-        unique:true
-     },
-     password:{
-        type:String,
-        required:true
-     },
-     role:{
-        type:String,
-        enum:['admin','user']
-     },
-     issuedbooks:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'issuedbooks'
-}],
+   name: {
+      type: String,
+      required: true
+   },
+   email: {
+      type: String,
+      required: true,
+      unique: true
+   },
+   password: {
+      type: String,
+      required: true
+   },
+   role: {
+      type: String,
+      enum: ['admin', 'user']
+   },
+   issuedbooks: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'issuedbooks'
+   }],
+  profile:Buffer,
 
-    
 
-createdAt:{
-    type:Date,
-    default:Date.now
-}
-    
-}, {timestamps:true}
+   createdAt: {
+      type: Date,
+      default: Date.now
+   }
+
+
+
+
+
+}, { timestamps: true }
 
 )
 
-const usermodel = mongoose.model('user',userschema)
+const usermodel = mongoose.model('user', userschema)
 module.exports = usermodel
 
