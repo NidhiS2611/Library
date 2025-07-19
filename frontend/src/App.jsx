e108a25e87f062fd52f5440b2dea999c65835819
Nidhi shakya <nidhikalpana555@gmail.com>
2025-07-19 19:40:09 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './component/Navbar.jsx'
 import Homepage from './pages/Homepage.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
+import { Navigate } from 'react-router-dom';
 
 import Protected from './component/Protected.jsx';
 import Librarydashboard from './layouts/Librarydashboard.jsx';
@@ -11,8 +12,11 @@ import IssuedBooks from './pages/IssuedBooks.jsx';
 import ReturnedBooks from './pages/Returnedbooks.jsx';
 import Userdashboard from './pages/Userdashboard.jsx';
 import Books from './pages/Books.jsx'
-
+import Profile from './pages/Profile.jsx';
+import Admindashboard from './layouts/Admindashboard.jsx';
+import Explore from './pages/Explore.jsx';
 function App() {
+
 
 
   return (
@@ -24,12 +28,17 @@ function App() {
           <Route path="/register" element={<Register />} />
 
           <Route path="/user" element={<Protected><Librarydashboard /></Protected>}>
-            <Route path="Userdashboard" element={<Userdashboard />} />
+            <Route index element={<Navigate to="dashboard" />} />
+              
+            <Route path="dashboard" element={<Userdashboard />} />
             <Route path="Books" element={<Books />} />
             <Route path="IssuedBooks" element={<IssuedBooks />} />
             <Route path="Returnedbooks" element={<ReturnedBooks />} />
+            <Route path="Profile" element={<Profile />} />
             
           </Route>
+          <Route path = '/admin' element = { <Admindashboard/>}/>
+          <Route path="/Explore" element={<Explore />} />
 
           
 
@@ -39,6 +48,7 @@ function App() {
 
 
       </Router>
+
     </>
   )
 }
