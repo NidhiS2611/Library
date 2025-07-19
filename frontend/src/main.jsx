e67db82135d215ba1e1be6e00ccabc45bd9bd294
Nidhi shakya <nidhikalpana555@gmail.com>
2025-07-19 19:40:26 +0530

    done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Darkmodeprovider } from './context/Darkmodecontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <Darkmodeprovider>
     <App />
+  </Darkmodeprovider>
   </StrictMode>,
 )
