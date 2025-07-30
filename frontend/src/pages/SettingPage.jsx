d7c4015ac4670536e58fb25bbb614ed8710262f3
Nidhi shakya <nidhikalpana555@gmail.com>
2025-07-30 11:32:22 +0530

    hv

--- frontend/src/pages/SettingPage.jsx
+++ frontend/src/pages/SettingPage.jsx
@@ -4,38 +4,38 @@ import { Link } from 'react-router-dom';
 const SettingsPage = () => {
   return (
     <div className="min-h-screen w-full bg-gray-100 dark:bg-gray-900 px-4 py-10 flex flex-col ">
-      <h2 className="text-3xl font-semibold text-center mb-10 dark:text-white">
+      <h2 className="text-3xl font-semibold  mb-10 dark:text-white">
         Settings
       </h2>
 
-      <div className="w-full max-w-md flex flex-col space-y-5">
+      <div className="w-full  flex flex-col space-y-5">
         <Link
           to="/accountsettings"
-          className="block px-6 py-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100 w-full"
+          className="block px-6 py-4  dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100 w-full"
         >
           Account Settings
         </Link>
         <Link
           to="/notificationsettings"
-          className="block px-6 py-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100 w-full"
+          className="block px-6 py-4  dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100 w-full"
         >
           Notification Settings
         </Link>
         <Link
           to="/securitysettings"
-          className="block px-6 py-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100 w-full"
+          className="block px-6 py-4 dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100 w-full"
         >
           Security Settings
         </Link>
         <Link
           to="/helpsettings"
-          className="block px-6 py-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100w-full"
+          className="block px-6 py-4  dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100w-full"
         >
           Help
         </Link>
         <Link
           to="/aisettings"
-          className="block px-6 py-4 bg-white dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100  w-full"
+          className="block px-6 py-4 dark:bg-gray-800 rounded-lg shadow hover:bg-blue-50 dark:hover:bg-gray-700 transition text-gray-900 dark:text-gray-100  w-full"
         >
           AI Settings
         </Link>
