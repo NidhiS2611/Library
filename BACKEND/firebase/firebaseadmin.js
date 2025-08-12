4fdc4ae939a2d38abc4acfd159cbe0fc3e39e3de
Nidhi shakya <nidhikalpana555@gmail.com>
2025-08-12 20:10:12 +0530

    done

--- BACKEND/firebase/firebaseadmin.js
+++ BACKEND/firebase/firebaseadmin.js
@@ -2,7 +2,7 @@
 const admin = require("firebase-admin");
 console.log('SERVICE_ACCOUNT:', process.env.SERVICE_ACCOUNT);
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 
 
 admin.initializeApp({
