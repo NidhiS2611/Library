1bba7f36870b8b8cb021b0a2245a209abde938ec
Nidhi shakya <nidhikalpana555@gmail.com>
2025-08-12 15:52:17 +0530

    done

--- BACKEND/firebase/firebaseadmin.js
+++ BACKEND/firebase/firebaseadmin.js
@@ -1,7 +1,7 @@
 // backend/firebase/firebaseAdmin.js
 const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
-console.log(process.env.SERVICE_ACCOUNT);
+console.log ('serviceaccount', process.env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
