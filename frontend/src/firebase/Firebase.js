54a691c1c5345e2937472b1b92230d76ab74fec6
Nidhi shakya <nidhikalpana555@gmail.com>
2025-08-13 11:32:05 +0530

    DONE

--- frontend/src/firebase/Firebase.js
+++ frontend/src/firebase/Firebase.js
@@ -6,12 +6,12 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID
 };
 
 // Initialize Firebase
