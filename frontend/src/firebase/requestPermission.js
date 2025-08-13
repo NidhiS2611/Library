58b2742590510a131762d9b66dc6dc929fe87c73
Nidhi shakya <nidhikalpana555@gmail.com>
2025-08-13 19:29:19 +0530

    DONE

--- frontend/src/firebase/requestPermission.js
+++ frontend/src/firebase/requestPermission.js
@@ -15,7 +15,7 @@ export const requestPermissionAndGetToken = async () => {
       console.log("✅ User FCM Token:", token);
 
       // Send token to backend
-      await axios.post('http://localhost:3000/user/save-token', { token }, { withCredentials: true });
+      await axios.post('https://librarymanagement-81b2.onrender.com/user/save-token', { token }, { withCredentials: true });
 
     } else {
       console.warn("❌ Notification permission not granted");
