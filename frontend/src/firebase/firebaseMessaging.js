8c53b2a22aaa11f03fbc3db08aa6fac4898fa60e
Nidhi shakya <nidhikalpana555@gmail.com>
2025-08-14 08:53:21 +0530

    done

--- frontend/src/firebase/firebaseMessaging.js
+++ frontend/src/firebase/firebaseMessaging.js
@@ -6,12 +6,8 @@ export const onMessageListener = () =>
   new Promise((resolve) => {
     onMessage(messaging, (payload) => {
       console.log('Foreground Message Received: ', payload);
-         if (Notification.permission === "granted") {
-        new Notification(payload.notification.title, {
-          body: payload.notification.body,
-          icon: '/logo192.png' // public folder ka path
-        });
-      }
+     
+      
       resolve(payload);
     });
   });
